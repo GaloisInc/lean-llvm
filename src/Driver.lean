59bb765c50c0bf5e10bb975081998c6d061707d2
Andrew Kent <andrew@galois.com>
2020-06-30 19:18:34 +0000

    feature: ppLLVM function for quick/easy pp-ing

--- src/Driver.lean
+++ src/Driver.lean
@@ -11,38 +11,38 @@ import LeanLLVM.LLVMOutput
 import LeanLLVM.SimMonad
 import LeanLLVM.Simulate
 
-open llvm.
+open LLVM.
 
 def readmainActual (x:String) : IO UInt32 := do
-  ctx ← ffi.newContext;
-  mb ← ffi.newMemoryBufferFromFile x;
-  m ← ffi.parseBitcodeFile mb ctx >>= loadModule;
-  dl <- match computeDataLayout m.data_layout with
+  ctx ← FFI.newContext;
+  mb ← FFI.newMemoryBufferFromFile x;
+  m ← FFI.parseBitcodeFile mb ctx >>= loadModule;
+  dl <- match computeDataLayout m.dataLayout with
         | (Except.error msg) => throw (IO.userError msg)
         | (Except.ok dl) => pure dl;
 
-  IO.println (pp.render (pp_module m));
+  IO.println (ppLLVM m);
 
   st0 <- runInitializers m dl
-             [(symbol.mk "arr", bitvec.of_nat 64 0x202000) ];
+             [(Symbol.mk "arr", bitvec.of_nat 64 0x202000) ];
 
   --let st0 := initializeState m dl;
 
   -- let res :=
-  --    sim.runFunc (symbol.mk "add_offset")
+  --    sim.runFunc (Symbol.mk "add_offset")
   --            [ sim.value.bv 64 (bv.from_nat 64 8)
   --            , sim.value.bv 32 (bv.from_nat 32 8)
   --            ]
   --            st0;
 
   res <-
-     sim.runFuncPrintTrace (symbol.mk "run_test")
+     Sim.runFuncPrintTrace (Symbol.mk "run_test")
              [
              ]
              st0;
 
   match res with
-  | (sim.value.bv _ x, _) =>
+  | (Sim.Value.bv x, _) =>
        do IO.println ("0x" ++ (Nat.toDigits 16 x.to_nat).asString);
           pure 0
   | _ => pure 0
@@ -52,8 +52,8 @@ def readmain (xs : List String) : IO UInt32 :=
   | [x] => readmainActual x
   | _ => throw (IO.userError "expected a single command line argument")
 
-def testModule : llvm.module :=
-  llvm.module.mk
+def testModule : LLVM.Module :=
+  LLVM.Module.mk
      (some "testmodule")
      []
      Array.empty -- types
@@ -68,9 +68,9 @@ def testModule : llvm.module :=
 
 
 def buildmain (xs : List String) : IO UInt32 := do
-  ctx <- ffi.newContext;
-  (_,mod) <- output.run (outputModule ctx testModule);
-  ffi.printModule mod;
+  ctx <- FFI.newContext;
+  (_,mod) <- Output.run (outputModule ctx testModule);
+  FFI.printModule mod;
   pure 0
 
 --def main := buildmain
